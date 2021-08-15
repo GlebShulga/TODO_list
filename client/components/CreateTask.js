0a8cb067fd3b214af0d1d6d52feb560f26f33117
GlebShulga <shulga_gleb@hotmail.com>
2021-08-15 12:56:27 +1000

    css refactoring

--- client/components/CreateTask.js
+++ client/components/CreateTask.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addTask } from '../redux/reducers/tasks'
-import plus from '../assets/images/plus.svg'
 
 const CreateTask = () => {
   const [title, setTitle] = useState('')
@@ -36,32 +35,13 @@ const CreateTask = () => {
           onKeyPress={handleKeypress}
         />
         {!error && (
-          <button
-            type="button"
-            className="py-3 px-4 bg-green-500 font-semibold uppercase hover:bg-green-400"
-            onClick={onClickAddTask}
-          >
-            <img src={plus} className="h-6 w-6 inline-block" alt="Plus Icon" />
+          <button type="button" className="addTaskButton" onClick={onClickAddTask}>
+            +
           </button>
         )}
         {error && (
-          <button
-            type="button"
-            className="py-3 px-4 bg-green-500 font-semibold uppercase hover:bg-green-400 opacity-50"
-            disabled
-          >
-            <svg
-              viewBox="0 0 20 20"
-              enableBackground="new 0 0 20 20"
-              className="w-6 h-6 inline-block"
-            >
-              <path
-                fill="#FFFFFF"
-                d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
-                                    C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
-                                    C15.952,9,16,9.447,16,10z"
-              />
-            </svg>
+          <button type="button" className="addTaskButton opacity-50" disabled>
+            +
           </button>
         )}
       </div>
